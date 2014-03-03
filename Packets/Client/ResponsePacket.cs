c88451f669b9afd499af5e5a0c8a24972c7835e9
Vitalij <Aragas@users.noreply.github.com>
2014-03-02 22:54:34 -0800

    Create ResponsePacket client side

---
+++ Packets/Client/ResponsePacket.cs
@@ -0,0 +1,25 @@
+using CWrapped;
+
+namespace MineLib.Network.Packets.Client
+{
+    public struct ResponsePacket : IPacket
+    {
+        public string Response;
+
+        public const byte PacketID = 0x00;
+        public byte Id { get { return 0x00; } }
+
+        public void ReadPacket(ref Wrapped stream)
+        {
+            Response = stream.ReadString();
+        }
+
+        public void WritePacket(ref Wrapped stream)
+        {
+            stream.WriteVarInt(Id);
+            stream.WriteString(Response);
+            stream.Purge();
+        }
+
+    }
+}
